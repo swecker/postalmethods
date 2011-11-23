295620284f1000abf9d48c1c5dd86d694d47df60
Samuel Wecker <wecker.samuel@gmail.com>
2011-11-23 15:36:11 -0800

    add explicit letter sending option

--- lib/postalmethods/send_letter.rb
+++ lib/postalmethods/send_letter.rb
@@ -6,11 +6,24 @@ module PostalMethods
       raise PostalMethods::NoPreparationException unless self.prepared 
       ## push a letter over the api
       
-
-      
       self.document = doc
       rv = @rpc_driver.sendLetter(:APIKey => self.api_key, :FileExtension => self.document[:extension], 
                                   :FileBinaryData => self.document[:bytes], :MyDescription => description, :WorkMode => self.work_mode)
+      
+      status_code = rv.sendLetterResult.to_i
+      
+      if status_code > 0
+        return status_code
+      elsif API_STATUS_CODES.has_key?(status_code)
+        instance_eval("raise APIStatusCode#{status_code.to_s.gsub(/( |\-)/,'')}Exception")
+      end
+    end
+    
+    def send_letter_explicitly(opts)
+      raise PostalMethods::NoPreparationException unless self.prepared 
+      raise PostalMethods::OptionsNotHashException unless (opts.class == Hash)
+      
+      rv = @rpc_driver.sendLetter({:APIKey => self.api_key, :WorkMode => self.work_mode}.merge!(opts))
       
       status_code = rv.sendLetterResult.to_i
       
